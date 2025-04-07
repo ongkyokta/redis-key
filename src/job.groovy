280c2b9a6a27863169b1c34bca79468c1f3d2cd7
ongkyoktafian1 <ongky.johansyah@ajaib.co.id>
2025-04-07 14:54:20 +0700

    add folder selection

--- src/job.groovy
+++ src/job.groovy
@@ -1,16 +1,13 @@
-import groovy.json.JsonSlurper
-
-// Define parameters for the Jenkins job
 pipelineJob('ongky_test') {
     description('Jenkins Pipeline Job for ongky_test')
 
     parameters {
         // String parameter for the JIRA URL
         stringParam('JIRA_URL', '', 'Enter the JIRA URL')
-        
+
         // Choice parameter for selecting the project
         choiceParam('PROJECT', ['payment', 'coin', 'platform'], 'Select the project folder')
-        
+
         // Active Choices Reactive Reference Parameter for Redis Folder
         activeChoiceReactiveReferenceParam('REDIS_FOLDER') {
             description('Select Redis Folder based on selected Project')
@@ -19,35 +16,44 @@ pipelineJob('ongky_test') {
                 script("""
 import groovy.json.JsonSlurper
 
-// Dynamically use the selected project from the PROJECT parameter
+// Get the selected project from Jenkins parameters (e.g., payment, coin, platform)
+//def project = build.buildVariableResolver.resolve("PROJECT") // PROJECT is passed as a parameter
+
+// Construct the URL to fetch the contents of the selected project inside 'stg'
 def githubRepoUrl = 'https://api.github.com/repos/ongkyokta/redis-key/contents/'
-def tribeName = "stg/\${PROJECT}" // Dynamically use the selected project
+def tribeName = "stg/"+PROJECT // Dynamically use the selected project
 
 def url = githubRepoUrl + tribeName
 
 // Execute the curl command to fetch the directory contents from GitHub API
-def command = ['curl', '-L', '-s', url]
+def command = ['curl', '-L', '-s', url] // Curl command with flags
 def process = command.execute()
 
 // Parse the response from the GitHub API
 def jsonResponse = new JsonSlurper().parseText(process.text)
 
-def html = new StringBuilder()
-html.append("<select name='value'>")
+// Initialize an array to hold the folder names (keydb folders)
+def folderList = []
+html=
+'''
+<select name="value">
+'''
 
-// Loop through the response and collect the names of subdirectories
+// Loop through the response and collect the names of subdirectories (keydb folders)
 jsonResponse.each { item ->
-    if (item.type == 'dir') {
-        html.append("<option value='\${item.name}'>\${item.name}</option>" + "\\n")
+    if (item.type == 'dir') { // Only consider directories (keydb folders)
+        folderList.add(item.name) // Add the directory name to the list
+        html = html+"<option value="+item.name+">"+item.name+"</option>"+"\n"
     }
 }
 
-html.append("</select>")
-return html.toString()
+return html = html+'''
+</select>
+'''
 """)
                 fallbackScript("""
-return ["<option value='error'>Failed to fetch folders</option>"]
-""")
+                    return ["<option value='error'>Failed to fetch folders</option>"]
+                """)
             }
             referencedParameter('PROJECT')
         }
